afc387aa2a30b283fbd5cde8bc32a48dbc170689
Sergej Sintschilin <seregpie@gmail.com>
2018-11-18 14:35:48 +0100

    update demo

--- demo/script.js
+++ demo/script.js
@@ -4,31 +4,53 @@
 		el: '#app',
 
 		data: {
-			colorIndex: 0,
-			colorValues: [
-				'orange',
-				'green',
-				'purple',
-			],
+			colorValues: ['cyan', 'light-green', 'yellow'],
+			dirtyColorIndex: 0,
+			moving: false,
 			offsetX: 0,
 			offsetY: 0,
+			shaking: false,
 		},
 
 		computed: {
 			color: function() {
 				return this.colorValues[this.colorIndex];
 			},
+
+			colorIndex: {
+				get: function() {
+					return this.dirtyColorIndex % this.colorValues.length;
+				},
+				set: function(value) {
+					this.dirtyColorIndex = value;
+				},
+			},
 		},
 
 		methods: {
+			onHoldEnd: function() {
+				this.shaking = false;
+			},
+
+			onHoldStart: function() {
+				this.shaking = true;
+			},
+
 			onPan: function(event) {
 				this.offsetX += event.x - event.previousX;
 				this.offsetY += event.y - event.previousY;
 			},
 
+			onPanEnd: function() {
+				this.moving = false;
+			},
+
+			onPanStart: function() {
+				this.moving = true;
+			},
+
 			onTap: function() {
 				this.colorIndex++;
-				this.colorIndex %= this.colorValues.length;
 			},
 		},
 	});
